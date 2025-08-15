7368bed6cc6f8ec90ce945229ffd58bd37565ff2
river <1722561295@qq.com>
2025-08-15 19:02:48 +0800

    引用官方库 实现相关的管理

--- manager.go
+++ manager.go
@@ -1,39 +1,10 @@
-package main
-
-import (
-	"fmt"
-	"math/rand"
-	"os"
-	"os/exec"
-	"strconv"
-	"time"
-)
-
-func generateSecret() string {
-	b := make([]byte, 16)
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 16; i++ {
-		b[i] = byte(rand.Intn(256))
-	}
-	return fmt.Sprintf("%x", b)
-}
-
-func getPublicIP() string {
-	cmd := exec.Command("curl", "-s", "https://api.ipify.org")
-	out, err := cmd.Output()
-	if err != nil {
-		return "YOUR_SERVER_IP"
-	}
-	return string(out)
-}
-
 func InstallMTProxy() {
 	fmt.Println(Lang[CurrentLang]["install"])
 
 	if _, err := os.Stat("MTProxy"); os.IsNotExist(err) {
-		if _, err := os.Stat("mtproxy-src.tar.gz"); os.IsNotExist(err) {
+		if _, err := os.Stat("mtproxy-master.zip"); os.IsNotExist(err) {
 			fmt.Println("Downloading MTProxy source code...")
-			cmd := exec.Command("wget", "-O", "mtproxy-src.tar.gz", "https://github.com/TelegramMessenger/MTProxy/archive/refs/heads/master.zip")
+			cmd := exec.Command("wget", "-O", "mtproxy-master.zip", "https://github.com/TelegramMessenger/MTProxy/archive/refs/heads/master.zip")
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			err := cmd.Run()
@@ -44,10 +15,14 @@ func InstallMTProxy() {
 		}
 
 		fmt.Println("Extracting MTProxy source...")
-		cmd := exec.Command("tar", "-xzf", "mtproxy-src.tar.gz")
+		cmd := exec.Command("unzip", "-q", "mtproxy-master.zip")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		cmd.Run()
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("解压失败，请安装 unzip 工具:", err)
+			return
+		}
 
 		os.Rename("MTProxy-master", "MTProxy")
 	}
@@ -58,53 +33,4 @@ func InstallMTProxy() {
 	cmd.Stderr = os.Stderr
 	cmd.Run()
 	fmt.Println("MTProxy 安装完成，可执行文件路径: MTProxy/objs/bin/mtproto-proxy")
-}
-
-func UninstallMTProxy() {
-	fmt.Println(Lang[CurrentLang]["uninstall"])
-	StopProxy()
-	os.RemoveAll("MTProxy")
-	fmt.Println("卸载完成")
-}
-
-func StartProxy(cfg *Config) {
-	if cfg.Secret == "" {
-		cfg.Secret = generateSecret()
-		SaveConfig(cfg)
-	}
-	cmd := exec.Command(cfg.Path,
-		"-p", strconv.Itoa(cfg.Port),
-		"-S", cfg.Secret,
-		"--aes-pwd", "proxy-secret", "/dev/null",
-		"-M", "1")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Start()
-	fmt.Println(Lang[CurrentLang]["start"])
-	fmt.Printf(Lang[CurrentLang]["link"]+"\n", getPublicIP(), cfg.Port, cfg.Secret)
-}
-
-func StopProxy() {
-	exec.Command("pkill", "-f", "mtproto-proxy").Run()
-	fmt.Println(Lang[CurrentLang]["stop"])
-}
-
-func RestartProxy(cfg *Config) {
-	StopProxy()
-	StartProxy(cfg)
-	fmt.Println(Lang[CurrentLang]["restart"])
-}
-
-func SetPort(cfg *Config, port int) {
-	cfg.Port = port
-	SaveConfig(cfg)
-	fmt.Println(Lang[CurrentLang]["setport"], port)
-	RestartProxy(cfg)
-}
-
-func SetSecret(cfg *Config, secret string) {
-	cfg.Secret = secret
-	SaveConfig(cfg)
-	fmt.Println(Lang[CurrentLang]["setkey"], secret)
-	RestartProxy(cfg)
 }
